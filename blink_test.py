fd3d907c1428a7e53fd415e2ef1c6c80dc91a7ee
Alexander Gattringer <a.gattringer@live.at>
2016-05-12 09:25:08 +0200

    changed blink test script

--- blink_test.py
+++ blink_test.py
@@ -1,12 +1,11 @@
+
 #!/usr/bin/env python
 
 import time
 
 import RPi.GPIO as GPIO
 
-BLINK_FREQ = 1  # check mail every 60 seconds
-
-green_on = False
+BLINK_FREQ = 1  # blink every 1 sec
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -16,22 +15,20 @@ GPIO.setup(GREEN_LED, GPIO.OUT)
 GPIO.setup(RED_LED, GPIO.OUT)
 
 def loop():
-    
-    if !green_on:
-        GPIO.output(GREEN_LED, True)
-        GPIO.output(RED_LED, False)
-    else:
-        GPIO.output(GREEN_LED, False)
-        GPIO.output(RED_LED, True)
+    GPIO.output(GREEN_LED, True)
+    GPIO.output(RED_LED, False)
 
-    green_on = !green_on
+    time.sleep(BLINK_FREQ)
+
+    GPIO.output(GREEN_LED, False)
+    GPIO.output(RED_LED, True)
 
     time.sleep(BLINK_FREQ)
 
 if __name__ == '__main__':
     try:
         print 'Press Ctrl-C to quit.'
-        while True:
+	while True:
             loop()
     finally:
         GPIO.cleanup()
